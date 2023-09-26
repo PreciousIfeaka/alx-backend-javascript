ee6978b5f8860be8ebf7a30124a93599ef518a21
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-09-26 00:08:09 +0000

    scoped given variables correctly

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
+}
