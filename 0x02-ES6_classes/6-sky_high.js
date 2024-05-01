a500c545a6693c9d748c26197b051ca065c49817
PreciousIfeaka <preciousifeaka@gmail.com>
2024-05-01 16:02:08 +0100

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+export default class skyHighBuilding extends Building {
+  constructor(sqft, floors = 0) {
+    super(sqft);
+    this._sqft = sqft;
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
