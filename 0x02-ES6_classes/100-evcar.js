8b8f481016165a9ba4a39b1786068eb314a14382
PreciousIfeaka <preciousifeaka@gmail.com>
2024-05-01 18:53:45 +0100

    fixed eslint issues

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,10 +1,12 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
+
+  // eslint-disable-next-line
 
   cloneCar() {
     return new Car[Symbol.species]();
