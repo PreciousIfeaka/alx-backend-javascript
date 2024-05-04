7c0311370448c01e55c505550c0f1e4f62f5bd67
PreciousIfeaka <preciousifeaka@gmail.com>
2024-05-04 12:13:51 +0100

    corrected this file

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(map) {
- if (!(map instanceof Map)) {
-  throw new Error('Cannot process');
- } else {
-  for (const [key, value] of map) {
-    if (value === 1) {
-      map.set(key, 100);
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  } else {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
     }
-  }
-  return map;
+    return map;
   }
 }
