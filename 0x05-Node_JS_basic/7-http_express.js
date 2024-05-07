ae2834c63c4fee06b67e3a17b9c4ed39ca067921
PreciousIfeaka <preciousifeaka@gmail.com>
2024-05-07 19:11:45 +0100

    complex express server

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,63 +1,27 @@
+const process = require('process');
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
-const { readFile } = require('fs');
+const port = 1245;
+const path = process.argv[2];
 
 const app = express();
-const port = 1245;
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
-  return new Promise((resolve, reject) => {
-    readFile(fileName, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        let output = '';
-        const lines = data.toString().split('\n');
-        for (let i = 0; i < lines.length; i += 1) {
-          if (lines[i]) {
-            length += 1;
-            const field = lines[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-              students[field[3]].push(field[0]);
-            } else {
-              students[field[3]] = [field[0]];
-            }
-            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-              fields[field[3]] += 1;
-            } else {
-              fields[field[3]] = 1;
-            }
-          }
-        }
-        const l = length - 1;
-        output += `Number of students: ${l}\n`;
-        for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {
-            output += `Number of students in ${key}: ${value}. `;
-            output += `List: ${students[key].join(', ')}\n`;
-          }
-        }
-        resolve(output);
-      }
-    });
-  });
-}
-
-app.get('/', (request, response) => {
-  response.send('Hello Holberton School!');
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.send('Hello Holberton School!');
 });
-app.get('/students', (request, response) => {
-  countStudents(process.argv[2].toString()).then((output) => {
-    response.send(['This is the list of our students', output].join('\n'));
+
+app.get('/students', (req, res) => {
+  countStudents(path).then((data) => {
+    res.send(`This is the List of our students\n${data}`);
   }).catch(() => {
-    response.send('This is the list of our students\nCannot load the database');
+    res.status(404).send('Cannot load database\nThis is the list of our students');
   });
 });
 
 app.listen(port, () => {
+
 });
 
 module.exports = app;
