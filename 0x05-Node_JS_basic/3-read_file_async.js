16a8f4a9473b84a3b6f50763c1373eebeb253ffb
PreciousIfeaka <preciousifeaka@gmail.com>
2024-05-08 05:32:10 +0100

    corrected test errors

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,56 +4,60 @@ const fs = require('fs');
 function countStudents(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, students) => {
-    if (err) {
-      reject(new Error('Cannot load database'));
-    } else {
-      let printResult = [];
-      students = students.toString().split('\r\n'); // This code was run on windows OS hence the /r
-      let studentsSize = students.length - 1;
-      let printStudentSize = `Number of students: ${studentsSize}`;
-      console.log(printStudentSize);
-      printResult.push(printStudentSize);
-
-
-      const data = [];
-      students.forEach((student, index) => {
-        if (index === 0) {
-          return;
-        } else {
-          student = student.split(',');
-          data.push(student);
+      if (err) {
+        reject(Error('Cannot load database'));
+      } else {
+        const printResult = [];
+        students = students.toString().split('\r\n'); // This code was run on windows OS hence the /r
+        let studentsSize = students.length - 1;
+        if (students[studentsSize] === '') {
+          students.pop();
+          studentsSize -= 1;
         }
-      })
-      
-      const groupedNames = {};
-      let fields = [];
+        const printStudentSize = `Number of students: ${studentsSize}`;
+        //console.log(printStudentSize);
+        printResult.push(printStudentSize);
 
-      data.forEach(entry => {
-        const [firstName, lastName, ,field] = entry; // Select the the firstname, lastname and field of each data entry
-        if (!fields.includes(field)) {
-          fields.push(field);
-        }
-        if (!groupedNames[field]) {
-          groupedNames[field] = [];
-        }
-        if (!groupedNames[field].includes(firstName)) {
-          groupedNames[field].push(firstName);
-        }
-      });
+        const data = [];
+        students.forEach((student, index) => {
+          if (index === 0) {
+            return;
+          } else {
+            student = student.split(',');
+            data.push(student);
+          }
+        });
 
-      sizeField1 = groupedNames[fields[0]].length;
-      sizeField2 = groupedNames[fields[1]].length;
+        const groupedNames = {};
+        const fields = [];
 
+        data.forEach((entry) => {
+          const [firstName, lastName, , field] = entry; // Select the the firstname, lastname and field of each data entry
+          if (!fields.includes(field)) {
+            fields.push(field);
+          }
+          if (!groupedNames[field]) {
+            groupedNames[field] = [];
+          }
+          if (!groupedNames[field].includes(firstName)) {
+            groupedNames[field].push(firstName);
+          }
+        });
 
-      result1 = `Number of students in ${fields[0]}: ${sizeField1}. List: ${groupedNames[fields[0]].join(', ')}`;
-      result2 = `Number of students in ${fields[1]}: ${sizeField2}. List: ${groupedNames[fields[1]].join(', ')}`;
-      console.log(`${result1}\n${result2}`);
+        sizeField1 = groupedNames[fields[0]].length;
+        sizeField2 = groupedNames[fields[1]].length;
 
-      printResult.push(result1);
-      printResult.push(result2);
-      resolve(printResult);
-    }
+        result1 = `Number of students in ${fields[0]}: ${sizeField1}. List: ${groupedNames[fields[0]].join(', ')}`;
+        result2 = `Number of students in ${fields[1]}: ${sizeField2}. List: ${groupedNames[fields[1]].join(', ')}`;
+        //console.log(`${result1}\n${result2}`);
+
+        printResult.push(result1);
+        printResult.push(result2);
+        resultParsed = printResult.join('\n');
+        //console.log(resultParsed);
+        resolve(resultParsed);
+      }
+    });
   });
-});
 }
 module.exports = countStudents;
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -14,9 +14,9 @@ app.get('/', (req, res) => {
 
 app.get('/students', (req, res) => {
   countStudents(path).then((data) => {
-    res.send(`This is the List of our students\n${data}`);
+    res.send(`This is the list of our students\n${data}`);
   }).catch(() => {
-    res.status(404).send('Cannot load database\nThis is the list of our students');
+    res.status(404).send('This is the list of our students\nCannot load the database');
   });
 });
 
