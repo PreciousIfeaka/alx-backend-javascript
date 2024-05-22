c9f1f2989479535cb64ae73c86fd7000d3bdb3a5
PreciousIfeaka <preciousifeaka@gmail.com>
2024-05-22 15:08:32 +0100

    corrected first test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,21 +4,24 @@ const calculateNumber = require("./0-calcul");
 
 describe('calculateNumber', () => {
   it('calculateNumber should return rounded sum of its arguments', () => {
-    assert.equal(calculateNumber(1, 3.7), Math.round(1) + Math.round(3.7));
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
   it('calculateNumber should return rounded sum of its arguments', () => {
-    assert.equal(calculateNumber(1, 3), Math.round(1) + Math.round(3));
+    assert.equal(calculateNumber(1, 3), 4);
   });
   it('calculateNumber should return rounded sum of its arguments', () => {
-    assert.equal(calculateNumber(1.2, 3.7), Math.round(1.2) + Math.round(3.7));
+    assert.equal(calculateNumber(1.2, 4), 5);
   });
   it('calculateNumber should return rounded sum of its arguments', () => {
-    assert.equal(calculateNumber(1.5, 3.7), Math.round(1.5) + Math.round(3.7));
+    assert.equal(calculateNumber(-1.5, 3.7), 3);
   });
   it('calculateNumber should return rounded sum of its arguments', () => {
-    assert.equal(calculateNumber(0.3, 0.7), Math.round(0.3) + Math.round(0.7));
+    assert.equal(calculateNumber(3.7, -1.5), 3);
   });
   it('calculateNumber should return rounded sum of its arguments', () => {
-    assert.equal(calculateNumber(2, 3.7), Math.round(2) + Math.round(3.7));
+    assert.equal(calculateNumber(2, 3.7), 6);
+  });
+  it('calculateNumber should return rounded sum of its arguments', () => {
+    assert.equal(calculateNumber(-3.7, -1.2), -5);
   });
 });
