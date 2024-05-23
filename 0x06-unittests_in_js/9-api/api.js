f4b902d5ff80258412add40d1eb8f17f9a37766b
PreciousIfeaka <preciousifeaka@gmail.com>
2024-05-23 09:47:01 +0100

    updated tests

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.status(200).send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
+app.get('/cart/:id(\\[0-9]*)', (req, res) => {
   res.status(200).send(`Payment methods for cart ${req.params.id}`);
 })
 
