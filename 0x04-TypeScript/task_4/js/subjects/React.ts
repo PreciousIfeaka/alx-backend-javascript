38d454a6c76653b76869b4b7a5d90c3b0a1eefe2
PreciousIfeaka <preciousifeaka@gmail.com>
2024-07-16 19:48:30 +0100

    added interface types

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,15 @@
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number
+  }
+  export class React extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React"
+    }
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingReact <= 0) {
+        return "No available teacher";
+      } else {return `Available Teacher: ${this.teacher.firstName}`}
+    }
+  }
+}
